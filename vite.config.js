ae9d7d9dca202676f4229995703449c0f2159af9
elenadiazp <alumno.646500@ies-azarquiel.es>
2024-12-02 21:37:06 +0100

    2.1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "./src", // Cambia la raíz del proyecto a "src"
   build: {
     outDir: "../docs", // Los archivos construidos irán a la carpeta "docs"
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./src/index.html"), // Página principal
+        form: resolve(__dirname, "./src/formulario.html"), // Otra página
+      },
+    },
   },
+  base: "./",
 });
